06033f93336ce3955a2e04775a8cf0db4fee0a5c
PeterPancake695 <petrupaun55@gmail.com>
2025-08-16 14:09:37 +0300

    im a dumbass

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servotester/ServoRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servotester/ServoRobot.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.servotester;
 
 import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@Config
 public class ServoRobot {
     FtcDashboard dashboard;
     public static double servoPosition = 0.0;
